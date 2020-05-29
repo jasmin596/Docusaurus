eafa2a5d29522b31b045292ea80a934a8b427f0b
jasmin596 <skjasmin596@gmail.com>
2020-05-29 18:38:48 +0530

    ai added

---
+++ docs/ai/ml.md
@@ -0,0 +1,7 @@
+---
+id: ml
+title: Mechaine learing
+---
+
+This is a link to [another document.](doc3.md)  
+This is a link to an [external page.](http://www.example.com)
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,6 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    {doc: 'ai/ai-intro', label: 'Artificial Intelligence'},
     {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
